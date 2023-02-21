48f2780f726a5924d508a570b8778a356af0b341
Asus <emirand20>
2023-02-21 19:45:21 +0100

    delete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Aplicacion from './Components/Aplicacion'
-import Aplicacion1 from './Components/Aplicacion1'
-
-
 
 function App() {
   return (
     <div className="App">
-      <h1>3 EN RAYA</h1>
-      <Aplicacion />
-      <h1>*********</h1>
-      <Aplicacion1 />
+
     </div>
   );
 }
